d53e0d17be291489203d2c520639c28a6f6a4993
Farrukh <farrukhrazzaqbutt@gmail.com>
2022-09-06 11:02:29 +0400

    static folder changes

--- DjangoRestApi/urls.py
+++ DjangoRestApi/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from django.conf.urls import url, include 
 
@@ -21,3 +22,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('djangoApp.urls')),
 ]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.conf.urls.static import static
+
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
